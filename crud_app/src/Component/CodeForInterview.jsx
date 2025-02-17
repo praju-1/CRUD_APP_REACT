5e5e4e078a01950116371838eb5f04f86f1b9312
Praju-1 <prajaktatayde26@gmail.com>
2025-02-17 10:58:04 +0530

    Image updation

--- crud_app/src/Component/CodeForInterview.jsx
+++ crud_app/src/Component/CodeForInterview.jsx
@@ -4,7 +4,8 @@ import { Box, Typography, styled } from '@mui/material';
 
 import Youtube from '../Assets/Images/Crud_image.jpg';
 import InstaTele from '../Assets/Images/mongo.png';
-import NodeJs from '../Assets/Images/NodeJS.jpg'
+import NodeJs from '../Assets/Images/NodeJS.jpg';
+import ReactJS  from '../Assets/Images/ReactJS.jpg'
 
 const Header = styled(Box)`
     margin: 50px;
@@ -15,7 +16,7 @@ const Header = styled(Box)`
 
 const Image = styled('img')({
     width: '50%',
-    height: '50%',
+    height: 'auto',
     padding:'10px'
 });
 
@@ -24,10 +25,11 @@ const CodeForInterview = () => {
     return (
         <Header>
             <Typography variant="h4">We are Here to Perform CRUD operations</Typography>
-            <Box style={{display: 'flex'}}>
-                <Image src={Youtube} />
-                <Image src={InstaTele} />
-                <Image src={NodeJs}/>
+           <Box style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
+                <Image src={Youtube}style={{ width: '45%' }} />
+                <Image src={InstaTele} style={{ width: '45%' }}/>
+                <Image src={NodeJs} style={{ width: '45%' }} /> 
+                <Image src={ReactJS} style={{ width: '45%' }} /> 
             </Box>
         </Header>
     )
